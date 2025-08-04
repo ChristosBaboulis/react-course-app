482b1bdeabe0e845bc23dd0efeb5e62e1ed8d2e8
ChristosBaboulis <chrisb2603@gmail.com>
2025-08-04 22:25:12 +0300

    Accessing input fields using objects

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,10 +4,13 @@ const Form = () => {
   const nameRef = useRef<HTMLInputElement>(null);
   const ageRef = useRef<HTMLInputElement>(null);
 
+  const person = { name: "", age: 0 };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (nameRef.current !== null) console.log(nameRef.current.value);
-    if (ageRef.current !== null) console.log(ageRef.current.value);
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+    console.log("Submitted:", person);
   };
 
   return (
